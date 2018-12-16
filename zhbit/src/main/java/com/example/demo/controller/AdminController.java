26e457c7618807a49ba70c0927277bc9bdd0e3ae
he-cocao <nsgc870039@163.com>
2018-12-16 17:09:32 +0800

    这是第一次上传啊

---
+++ zhbit/src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+	// 访问地址便是 http://localhost:8081/admin/index
+	@RequestMapping("index")
+	public ModelAndView page1() {
+		return new ModelAndView("/index.html");
+	}
+
+	// http://localhost:8081/admin/map
+	@RequestMapping("map")
+	public Map<String, String> map1() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("name", "张三");
+		map.put("age", "28");
+		return map;
+	}
+	
+
+
+
+}
